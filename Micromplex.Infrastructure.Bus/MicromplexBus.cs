40f0323e88c7fa37ccc17eecda88446d35207f19
Loki <disrute@gmail.com>
2021-02-27 15:56:57 +1100

    Implement Bus send command

---
+++ Micromplex.Infrastructure.Bus/MicromplexBus.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Micromplex.Domain.Core.Bus;
+using Micromplex.Domain.Core.Commands;
+using Micromplex.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Micromplex.Infrastructure.Bus
+{
+    public sealed class MicromplexBus : IEventBus
+    {
+        private readonly IMediator _mediator;
+        private readonly Dictionary<string, List<Type>> _handlers;
+        private readonly List<Type> _eventTypes;
+
+        public MicromplexBus(IMediator mediator)
+        {
+            _mediator = mediator;
+            _handlers = new Dictionary<string, List<Type>>();
+            _eventTypes = new List<Type>();
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+
+        public void Publish<T>(T @event) where T : Event
+        {
+            
+        }
+
+        public void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>
+        {
+            
+        }
+    }
+}
